0f385ef639ff3cbccfb8a69f1a4aa68a18e5fd8a
sinsau182 <singhsaurav182001@gmail.com>
2024-11-30 16:20:04 +0530

    version 3.0

--- frontend/src/Components/Create.jsx
+++ frontend/src/Components/Create.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Create = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [age, setAge] = useState(0);
+
   return (
-    <div>
-      <h4>Create Post</h4>
+    <div className='container my-2'>
+
+        <h2 className='text-center'>Enter the Data</h2>
+
+        <form>
+        <div className="mb-3">
+    <label className="form-label">Name</label>
+    <input type="text" className="form-control" 
+    value={name}
+    onChange={(e) => setName(e.target.value)}/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Email address</label>
+    <input type="email" className="form-control" 
+    value={email}
+    onChange={(e) => setEmail(e.target.value)}/>
+  </div>
+  <div className="mb-3">
+    <label className="form-label">Age</label>
+    <input type="number" className="form-control" 
+    value={age} 
+    onChange={(e) => setAge(e.target.value)} />
+  </div>
+  <button type="submit" className="btn btn-primary">Submit</button>
+</form>
+      
     </div>
   )
 }
